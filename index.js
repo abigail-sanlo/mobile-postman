4636b606ec4ef4221057c42917fe7f0adaf583d6
Abisl <60681410+abigail-sanlo@users.noreply.github.com>
2026-05-28 06:07:42 +0000

    change logic

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const express = require('express');
-// const sqlite3 = require('sqlite3').verbose();
 const Database = require('better-sqlite3');
 const cors = require('cors');
 
@@ -8,49 +7,45 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// const db = new sqlite3.Database('./todos.db');
 const db = new Database('todos.db');
 
-db.serialize(() => {
-  db.run(`
-    CREATE TABLE IF NOT EXISTS todos (
-      id INTEGER PRIMARY KEY AUTOINCREMENT,
-      todo TEXT NOT NULL,
-      created_at TEXT
-    )
-  `);
-});
+db.prepare(`
+  CREATE TABLE IF NOT EXISTS todos (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    todo TEXT NOT NULL,
+    created_at TEXT
+  )
+`).run();
 
 app.post('/agrega_todo', (req, res) => {
   const { todo } = req.body;
 
   if (!todo) {
-    return res.status(400).json({ error: 'El campo todo es obligatorio' });
+    return res.status(400).json({
+      error: 'El campo todo es obligatorio'
+    });
   }
 
-  const query = `INSERT INTO todos (todo, created_at) VALUES (?, datetime('now'))`;
+  const stmt = db.prepare(`
+    INSERT INTO todos (todo, created_at)
+    VALUES (?, datetime('now'))
+  `);
 
-  db.run(query, [todo], function (err) {
-    if (err) {
-      return res.status(500).json({ error: err.message });
-    }
+  const result = stmt.run(todo);
 
-    res.status(201).json({
-      id: this.lastID,
-      todo
-    });
+  res.status(201).json({
+    id: result.lastInsertRowid,
+    todo
   });
 });
 
 app.get('/todos', (req, res) => {
-  db.all("SELECT * FROM todos", [], (err, rows) => {
-    if (err) {
-      return res.status(500).json({ error: err.message });
-    }
-    res.json(rows);
-  });
+  const todos = db.prepare('SELECT * FROM todos').all();
+  res.json(todos);
 });
 
-app.listen(3000, () => {
-  console.log('Servidor corriendo en http://localhost:3000');
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en puerto ${PORT}`);
 });
