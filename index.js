bf4eedd5392538af877004bb85fc72d48816da18
Abisl <60681410+abigail-sanlo@users.noreply.github.com>
2026-05-28 05:55:15 +0000

    change sql

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const sqlite3 = require('sqlite3').verbose();
+// const sqlite3 = require('sqlite3').verbose();
+const Database = require('better-sqlite3');
 const cors = require('cors');
 
 const app = express();
@@ -7,7 +8,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const db = new sqlite3.Database('./todos.db');
+// const db = new sqlite3.Database('./todos.db');
+const db = new Database('todos.db');
 
 db.serialize(() => {
   db.run(`
